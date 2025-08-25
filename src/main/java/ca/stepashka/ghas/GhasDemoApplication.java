db4df03c2f52b9b047437cd48341b26a70fd4ba0
Stepan Razdobarov <stepanrazdobarov@SRAZDOBAROV-MBP.local>
2025-08-25 11:54:55 -0600

    init

---
+++ src/main/java/ca/stepashka/ghas/GhasDemoApplication.java
@@ -0,0 +1,13 @@
+package ca.stepashka.ghas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhasDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/stepashka/ghas/GhasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.stepashka.ghas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
